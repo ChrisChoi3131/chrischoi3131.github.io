0f21c003a09d774f09ab229d3778d14201bdc151
Chris Choi <chrischoi3131@gmail.com>
2022-05-13 22:32:19 -0700

    deploy v0.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,29 @@
 module.exports = {
-  root: true,
   env: {
-    node: true,
     es2021: true,
+    browser: true,
+    node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-  ],
   parserOptions: {
-    // parser: "babel-eslint",
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
-  plugins: ["prettier"],
+  plugins: ['prettier', 'html'],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  settings: {
+    'html/report-bad-indent': 'error',
+    'html/html-extensions': ['.html'],
+  },
   rules: {
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
-        singleQuote: false,
-        semi: true,
+        printWidth: 120,
+        singleQuote: true,
         useTabs: false,
         tabWidth: 2,
-        trailingComma: "all",
-        printWidth: 80,
-        bracketSpacing: true,
-        arrowParens: "always",
-        htmlWhitespaceSensitivity: "ignore",
+        arrowParens: 'avoid',
       },
     ],
-    "import/order": "error",
-    "import/first": "error",
-    "import/no-mutable-exports": "error",
-    "import/no-unresolved": "off",
   },
 };
